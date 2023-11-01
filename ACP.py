e069534604c73acc4916636ac0a77abaaf719c46
rplewi <rlewi0506@gmail.com>
2023-11-01 10:37:45 -0500

    Added new file

--- ACP.py
+++ ACP.py
@@ -1,43 +1,8 @@
-# Author: 
-# Roman Lewis
-#
-# Project:
-# Add-Commit-Push automation
-import subprocess
-
-def git_status():
-    try:
-        result = subprocess.run(['git', 'status'], capture_output=True, text=True, check=True)
-        print(result.stdout)
-    except subprocess.CalledProcessError as e:
-        print(f"An error occurred: {e}")
-
-def git_add():
-    try:
-        add = subprocess.run(['git', 'add', '.'], capture_output=True, text=True, check=True)
-        print(add.stdout)
-    except subprocess.CalledProcessError as e:
-        print(f"An error occured: {e}")
-
-def git_message():
-    message = input('Enter Your Message: ')
-    print(message)
-def git_comment():
-    try:
-        message = input('Enter Your Message: ')
-        comment = subprocess.run(['git', 'commit', '-m' '"', + message, '"'], capture_output=True, text=True, check=True)
-        print(comment.stdout)
-    except subprocess.CalledProcessError as e:
-        print(f"An error occured: {e}")
-
-if __name__ == "__main__":
-    git_status()
-    git_add()
-    git_comment()
-
 import os
 
+print("Starting Code...")
 os.system('git status')
 os.system('git add .')
-os.system()
-
+message = input('Enter Your Message: ')
+os.system('git commit -m ' + message) 
+os.system('git push')
--- Testing-File.py
+++
@@ -1,8 +0,0 @@
-import os
-
-print("Starting Code...")
-os.system('git status')
-os.system('git add .')
-message = input('Enter Your Message: ')
-os.system('git commit -m ' + message) 
-os.system('git push')
