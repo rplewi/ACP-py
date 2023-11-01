9b24b845e8748885c43b10a8801eb7d7767dc6ff
rplewi <rlewi0506@gmail.com>
2023-11-01 10:41:20 -0500

    Testing

--- ACP.py
+++ ACP.py
@@ -2,7 +2,10 @@ import os
 
 print("Starting Code...")
 os.system('git status')
+print('git status')
 os.system('git add .')
+print('git add .')
 message = input('Enter Your Message: ')
 os.system('git commit -m ' + message) 
 os.system('git push')
+print('git push')
