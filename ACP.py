f70292efad93a30e147b803aba76abfb632b0eda
rplewi <rlewi0506@gmail.com>
2023-11-05 20:58:12 -0600

    f

--- ACP.py
+++ ACP.py
@@ -12,13 +12,14 @@ os.system('git add .')
 while True:
     message = input('Enter Your Message or f to bypass confirmation: ')
     os.system('git commit -m "' + message + '"') 
+    confirmMessage = input('Please Confirm that you want to push this branch (y or n): ' )
+    push()
     if not message:
         print('Please enter a valid message')
         continue
     elif message in force:
         push()
 
-    confirmMessage = input('Please Confirm that you want to push this branch (y or n): ' )
 
 def push():
     if confirmMessage in yes:
